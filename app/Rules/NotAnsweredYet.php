018e54d6593d2c211406ed9c888a11a4eee1068b
Flight <vladimirborisiuk@gmail.com>
2025-08-10 16:54:51 +0300

    Custom create answer rule;

---
+++ app/Rules/NotAnsweredYet.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use App\Models\Answer;
+
+class NotAnsweredYet implements ValidationRule
+{
+    public function __construct(
+        protected int $userId
+    )
+    {
+    }
+
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (Answer::where('guest_user_id', $this->userId)
+            ->where('question_id', $value)
+            ->exists()
+        ) {
+            $fail('You have already answered this question.');
+        }
+    }
+}
